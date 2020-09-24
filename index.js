9293047cda50dc42f0326749eaf5a8a90e481b91
Adria Vieira <adriavieira731@gmail.com>
2020-09-24 12:43:20 -0400

    CRUD atualizado para atender ao MongoDB

--- index.js
+++ index.js
@@ -24,73 +24,72 @@ const mongodb = require('mongodb');
     const jsonParser = bodyParser.json();
     app.use(jsonParser);
 
+    const db = await client.db('ocean_mongodb');
+    //Variável mensagens está recebendo a collection do banco de dados
+    const mensagens = await db.collection('mensagens');
+    
     //Endpoints de envio de mensagens
     //CRUD - Create, Read(Read All e Read Single), Update and Delete
-    const mensagens = [
-        {
-            id: 0,
-            texto: 'Cachorro quente'
-
-        },
-        {
-            id: 1,
-            texto: 'Sushi'
-        }  
-    ];
-
     app.get('/', (req, res) => {
-        res.send('Hello, Adria');
+        res.send('Hello World com MongoDB.');
     });
 
     //Read All
-    app.get('/mensagens', (req, res) => {
-        res.json(mensagens);    
+    app.get('/mensagens', async (req, res) => {
+        const find = await mensagens.find({}).toArray();
+        res.json(find);    
     });
 
     //Create
-    app.post('/mensagens', (req, res) => {
-        //Adquiro o objeto do corpo
+    app.post('/mensagens', async (req, res) => {
+        //Obtendo a mensagem do corpo da requisição
         const mensagem = req.body;
-        //Capturando o tamanho do objeto mensagens
-        const id = mensagens.length;
-        //adicionando o campo id na nova mensagem
-        mensagem.id = id;
-        //Insiro a mensagem na lista
-        mensagens.push(mensagem);
+        //OBS: MongoDB adiciona o ID sozinho
 
-        res.send(`A sua mensagem ${mensagem.texto} foi adicionada. ID: ${id}`);
+        //Resultado esta recebendo a chamada da collection que esta inserindo uma nova mensagem
+        const resultado = await mensagens.insertOne(mensagem);
+        //ops[0] é onde está o objeto inserido
+        const objetoInserido = resultado.ops[0];
+
+        //Devo colocar esse res para que o insomnia pare de rodar
+        res.json(objetoInserido);
     });
 
     //Read Single
-    app.get('/mensagens/:id', (req, res) => {
+    app.get('/mensagens/:id', async (req, res) => {
         //Acessando o id
         const id = req.params.id;
         //Identificando qual é a mensagem pelo o id
-        const mensagem = mensagens[id];
+        const mensagem = await mensagens.findOne({ _id: mongodb.ObjectId(id) });
 
-        res.json(`${mensagem.texto}`);
+        res.json(mensagem);
     });
 
     //Update
-    app.put('/mensagens/:id', (req, res) => {
+    app.put('/mensagens/:id', async (req, res) => {
         // Acessando o paramentro id
         const id = req.params.id;
         //Capturando a nova mensagem do body da requisição
-        const novoTexto = req.body.texto;
-        //Atualizando a nova mensagem ao objeto especificado pelo id
-        mensagens[id].texto = novoTexto;
+        const novoMensagem = req.body;
+        //Capturando a mensagemAtual do objeto identificado pelo id
+        const mensagemAtual = await mensagens.findOne({ _id: mongodb.ObjectId(id) });
+        //O campo texto de mensagemAtual vai receber o texto da novaMensagem enviada pelo body
+        mensagemAtual.texto = novoMensagem.texto;
+        //Atualizando a mensagem identificada pelo ID na collection mensagens diretamente
+        //com o novo texto de mensagemAtual
+        await mensagens.updateOne({ _id: mongodb.ObjectId(id) }, { $set: mensagemAtual });
 
-        res.send(`Mensagem com id ${id} foi atualizada. ${mensagens[id].texto}`);
+        res.send('Atualizado com sucesso!');
     });
 
     //Delete
-    app.delete('/mensagens/:id', (req, res) => {
+    app.delete('/mensagens/:id', async (req, res) => {
         // Acessando o paramentro id
         const id = req.params.id;
-        //Deltando a mensagem com o id selecionado
-        delete mensagens[id];
+        //Deletando a mensagem que foi identificada na collection pelo seu id
+        await mensagens.deleteOne({ _id: mongodb.ObjectId(id) });
 
-        res.send(`Mensagem de id ${id} apagada`);
+        res.send(`Mensagem de id ${id} deletada com sucesso!`);
     });
 
     //Ouvindo a porta e recebendo suas requisições
